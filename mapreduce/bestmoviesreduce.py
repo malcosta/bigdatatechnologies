a7c508fae53eae2aafbae593a2f36c7e3c580bf6
malcosta <62447252+malcosta@users.noreply.github.com>
2022-02-20 20:25:43 +0000

    Create bestmoviesreduce.py

---
+++ mapreduce/bestmoviesreduce.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import sys
+import csv
+
+TAB_CHAR = '\t'
+MIN_COUNT = 100
+
+
+def movie_title(movie_id, ratings):
+	'''
+	Convert from movie id to movie title
+	'''
+	with open("/root/input/u.item", "r") as infile:
+		reader = csv.reader(infile, delimiter='|')
+		next(reader)
+		sret = "NOT_FOUND"
+		for line in reader:
+			if int(movie_id) == int(line[0]):
+				sret = line[1]      
+	print(str(ratings) + TAB_CHAR + str(movie_id) + " , " + sret)
+
+
+last_id = None
+sum_ratings = 0
+count = 0
+for line in sys.stdin:
+	movie_id, rating = line.split(TAB_CHAR, 1)
+	if last_id and last_id != movie_id:
+		if count >= MIN_COUNT:
+			movie_title(int(last_id), sum_ratings/float(count))
+		sum_ratings = int(rating)
+		count = 1
+	else:
+		sum_ratings += int(rating)
+		count += 1
+	last_id = movie_id
+
+if count >= MIN_COUNT:
+	movie_title(int(last_id), sum_ratings/float(count))
